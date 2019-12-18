b6cd48a6a9affaffd0ba2e047fae62f6d1e133ac
jinilover <columbawong@gmail.com>
2019-12-18 20:02:23 +1100

    Revert "Revert "Move `MonadBSReader` type class to `Common`""
    This reverts commit 89e78d9ea3a58a2ee66588b3070c99428fd4a657.

--- src/lib/Parliament/Common.hs
+++ src/lib/Parliament/Common.hs
@@ -1,9 +1,20 @@
 module Parliament.Common where
 
 import Data.String (String)
+import qualified Data.ByteString.Lazy as B
 
 type BillId = Int
 
 type Category = String
 
 type DistrictName = String
+
+-- refer to https://chrispenner.ca/posts/monadio-considered-harmful
+class Monad m => MonadBSReader m where
+  readByteString :: FilePath -> m B.ByteString
+
+instance MonadBSReader IO where
+  readByteString = B.readFile
+    
+instance MonadBSReader m => MonadBSReader (ExceptT e m) where
+  readByteString = lift . readByteString
